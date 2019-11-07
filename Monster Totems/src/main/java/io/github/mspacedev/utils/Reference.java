381a02ff3e300370bc759edcf9bb40f7f331c7ec
MSpaceDev <mspacedev1@gmail.com>
2019-11-07 02:22:22 +0200

    Updated project to 1.12

--- Monster Totems/src/main/java/io/github/mspacedev/utils/Reference.java
+++ Monster Totems/src/main/java/io/github/mspacedev/utils/Reference.java
@@ -13,7 +13,7 @@ public class Reference
 {
 	public static final String NAME = "Monster Totems";
 	public static final String MODID = "monstertotems";
-	public static final String VERSION = "[1.10.2]";
+	public static final String VERSION = "[1.12]";
 	public static final String COMMON_PROXY = "io.github.mspacedev.proxies.CommonProxy";
 	public static final String CLIENT_PROXY = "io.github.mspacedev.proxies.ClientProxy";
 
